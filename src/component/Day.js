5467c4dbbd01283f8a06727fd35f4bf8f7f48d0f
Leesungsup <tmdtjq0116@naver.com>
2023-02-24 01:05:40 +0900

    Fetch

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,15 +1,26 @@
 import dummy from "../db/data.json";
 import {useParams} from "react-router-dom";
+import { useEffect,useState } from "react";
 import Word from "./Word";
+import useFetch from "../hooks/useFetch";
 function Day(){
+  // const [words,setWords] = useState([]);
   const {day} = useParams();
-  const wordList = dummy.words.filter(word=>word.day===Number);
+  // const wordList = dummy.words.filter(word=>word.day===Number);
+  const words=useFetch(`http://localhost:3001/words?day=${day}`);
+  // useEffect(()=>{
+  //   fetch(`http://localhost:3001/words?day=${day}`).then(res=>{
+  //     return res.json();
+  //   }).then(data=>{
+  //     setWords(data);
+  //   });
+  // },[day]);
   return(
     <div>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word=>(
+          {words.map(word=>(
             <Word word={word} key={word.id}/>
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,35 +1,42 @@
 import dummy from "../db/data.json";
 import {Link} from "react-router-dom"
 import {useEffect,useState} from "react";
+import useFetch from "../hooks/useFetch";
 function DayList(){
-  const  [count,setCount] = useState(0);
-  const [days,setDays] = useState([]);
-  function onClick(){
-    setCount(count+1);
-  }
-  function onClick2(){
-    setDays([
-      ...days,
-      {
-        id:Math.random(),
-        day:1,
-      }
-    ])
-  }
-  useEffect(()=>{
-    console.log("Count change");
-  },[count]);
+  // const  [count,setCount] = useState(0);
+  // const [days,setDays] = useState([]);
+  // function onClick(){
+  //   setCount(count+1);
+  // }
+  // function onClick2(){
+  //   setDays([
+  //     ...days,
+  //     {
+  //       id:Math.random(),
+  //       day:1,
+  //     }
+  //   ])
+  // }
+
+  const days=useFetch("http://localhost:3001/days")
+  // useEffect(()=>{
+  //   fetch("http://localhost:3001/days").then(res=>{
+  //     return res.json();
+  //   }).then(data=>{
+  //     setDays(data);
+  //   });
+  // },[]);
   return(
     <div>
       <ul className="list_day">
-        {dummy.days.map(day=>(
+        {days.map(day=>(
           <li key={day.id}>
             <Link to={`/day/${day.day}`}>Day {day.day}</Link>
             </li>
         ))}
       </ul>
-      <button onClick={onClick}>{count}</button>
-      <button onClick={onClick2}>Day change</button>
+      {/* <button onClick={onClick}>{count}</button>
+      <button onClick={onClick2}>Day change</button> */}
     </div>
   )
 };
